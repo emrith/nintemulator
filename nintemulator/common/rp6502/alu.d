69df2c2a33b63943c2faf09021b4b7ee0a93f16f
Adam Becker <beannaich@gmail.com>
2015-07-26 14:45:31 -0600

    Updating the RP6502 ALU code.

--- nintemulator/common/rp6502/alu.d
+++ nintemulator/common/rp6502/alu.d
@@ -6,6 +6,54 @@ class ALU
     public ubyte c;
     public ubyte v;
 
+    public ubyte add(ubyte lhs, ubyte rhs, int carry = 0)
+    {
+        asm
+        {
+            ror carry, 1;
+            mov ECX, EAX;
+            mov AL, lhs;
+            adc AL, rhs;
+            setc c[ECX];
+            seto v[ECX];
+        }
+    }
+
+    public ubyte sub(ubyte lhs, ubyte rhs, int carry = 0)
+    {
+        return add(lhs, ~rhs, carry);
+    }
+
+    unittest
+    {
+        auto sut = new ALU();
+
+        assert(sut.add(0xff, 0x00, 0) == 0xff);
+        assert(sut.add(0xff, 0x00, 1) == 0x00);
+        assert(sut.add(0xff, 0x01, 0) == 0x00);
+        assert(sut.add(0xff, 0x01, 1) == 0x01);
+
+        sut.c = 0;
+        sut.add(0xff, 0x00, 1);
+
+        assert(sut.c == 1);
+
+        sut.c = 1;
+        sut.add(0xff, 0x00, 0);
+
+        assert(sut.c == 0);
+
+        sut.v = 0;
+        sut.add(0x80, 0x80, 0);
+
+        assert(sut.v == 1);
+
+        sut.v = 0;
+        sut.add(0x80, 0x7f, 1);
+
+        assert(sut.v == 0);
+    }
+
     public ubyte shl(ubyte value, int carry = 0)
     {
         asm
@@ -22,20 +70,20 @@ class ALU
     {
         auto sut = new ALU();
 
-        assert( sut.shl( 0x80, 0 ) == 0x00 );
-        assert( sut.shl( 0x40, 0 ) == 0x80 );
-        assert( sut.shl( 0x80, 1 ) == 0x01 );
-        assert( sut.shl( 0x40, 1 ) == 0x81 );
+        assert(sut.shl(0x80, 0) == 0x00);
+        assert(sut.shl(0x40, 0) == 0x80);
+        assert(sut.shl(0x80, 1) == 0x01);
+        assert(sut.shl(0x40, 1) == 0x81);
 
         sut.c = 0;
-        sut.shl( 0x80, 0 );
+        sut.shl(0x80, 0);
 
-        assert( sut.c == 1 );
+        assert(sut.c == 1);
 
         sut.c = 1;
-        sut.shl( 0x40, 0 );
+        sut.shl(0x40, 0);
 
-        assert( sut.c == 0 );
+        assert(sut.c == 0);
     }
 
     public ubyte shr(ubyte value, int carry = 0)
@@ -54,19 +102,19 @@ class ALU
     {
         auto sut = new ALU();
 
-        assert( sut.shr( 0x01, 0 ) == 0x00 );
-        assert( sut.shr( 0x80, 0 ) == 0x40 );
-        assert( sut.shr( 0x01, 1 ) == 0x80 );
-        assert( sut.shr( 0x80, 1 ) == 0xc0 );
+        assert(sut.shr(0x01, 0) == 0x00);
+        assert(sut.shr(0x80, 0) == 0x40);
+        assert(sut.shr(0x01, 1) == 0x80);
+        assert(sut.shr(0x80, 1) == 0xc0);
 
         sut.c = 0;
-        sut.shr( 0x01, 0 );
+        sut.shr(0x01, 0);
 
-        assert( sut.c == 1 );
+        assert(sut.c == 1);
 
         sut.c = 1;
-        sut.shr( 0x80, 0 );
+        sut.shr(0x80, 0);
 
-        assert( sut.c == 0 );
+        assert(sut.c == 0);
     }
 }
